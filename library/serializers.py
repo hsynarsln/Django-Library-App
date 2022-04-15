21a29b37bdd1dbb48ee0848e5fad6b18c14685f0
hsynarslan <hsyn06arslan@gmail.com>
2022-04-15 14:09:12 +0300

    pagination, filter

--- library/serializers.py
+++ library/serializers.py
@@ -6,13 +6,21 @@ from .models import Author, Book, Category, Publisher
 
 
 class BookSerializer(serializers.ModelSerializer):
-    # category = serializers.StringRelatedField()
-    # author = serializers.StringRelatedField()
-    # publisher = serializers.StringRelatedField()
+    category = serializers.PrimaryKeyRelatedField(
+        write_only=True, queryset=Category.objects.all())
+    category_name = serializers.CharField(
+        source='category.name', read_only=True)
+    author = serializers.PrimaryKeyRelatedField(
+        write_only=True, queryset=Author.objects.all())
+    author_name = serializers.CharField(source='author.name', read_only=True)
+    publisher = serializers.PrimaryKeyRelatedField(
+        write_only=True, queryset=Publisher.objects.all())
+    publisher_name = serializers.CharField(
+        source='publisher.name', read_only=True)
 
     class Meta:
         model = Book
-        fields = ('id', 'title', 'category', 'author', 'publisher', 'published_date',
+        fields = ('id', 'title', 'category', 'category_name', 'author', 'author_name', 'publisher', 'publisher_name', 'published_date',
                   'total_pages', 'description', 'created_date', 'updated_date')
 
 
--- library/views.py
+++ library/views.py
@@ -1,7 +1,9 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status, viewsets
 from rest_framework.response import Response
 
 from .models import Author, Book, Category, Publisher
+from .pagination import Pagination
 from .serializers import (AuthorSerializer, BookSerializer, CategorySerializer,
                           PublisherSerializer)
 
@@ -9,6 +11,15 @@ from .serializers import (AuthorSerializer, BookSerializer, CategorySerializer,
 class BookView(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    pagination_class = Pagination
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['category', 'author', 'publisher']
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        content = {'message': 'Book deleted!'}
+        return Response(content, status=status.HTTP_204_NO_CONTENT)
 
 
 class CategoryView(viewsets.ModelViewSet):
